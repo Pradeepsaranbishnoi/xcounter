8133033cb579467542a6328fa6fb032cdd877dd7
Pradeep Saran <pradeepmadasar@gmail.com>
2025-07-05 22:36:54 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import React, { useState } from "react";
+import Calculator from "./Calculator";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const increment = () => setCount(prev => prev + 1);
-  const decrement = () => setCount(prev => prev - 1);
-
   return (
     <div className="App">
-      <h1>Count: {count}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <Calculator/>
     </div>
   );
 }
